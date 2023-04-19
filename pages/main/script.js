634f6063c40c13901c5ed4a0b4c294bd006512f8
Julia Baulina <y.nevera@gmail.com>
2023-04-19 18:01:49 +0200

    fix: changa panda panel width

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,16 +1,36 @@
-const headerNavList = document.querySelectorAll(".header-nav-list > li")
-const footerNavList = document.querySelectorAll(".footer-nav-list > li")
+const headerNavList = document.querySelectorAll(".header-nav-list > li");
+const footerNavList = document.querySelectorAll(".footer-nav-list > li");
 
+headerNavList.forEach((element) => {
+  element.addEventListener("click", () => {
+    headerNavList.forEach((el) => el.classList.remove("highlighted"));
+    element.classList.add("highlighted");
+  });
+});
+footerNavList.forEach((element) => {
+  element.addEventListener("click", () => {
+    footerNavList.forEach((el) => el.classList.remove("highlighted"));
+    element.classList.add("highlighted");
+  });
+});
 
-headerNavList.forEach(element => {
-    element.addEventListener("click", () => {
-        headerNavList.forEach(el => el.classList.remove("highlighted"));
-        element.classList.add("highlighted");
-    })
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+let cardsCollection = document.querySelectorAll(".animal-card");
+const cardsCollectionContainer = document.querySelector(".animal-cards");
+
+function clearCards() {
+  cardsCollection.forEach((card) => {
+    card.remove();
+  });
+}
+
+arrowLeft.addEventListener("click", (e) => {
+  clearCards();
 });
-footerNavList.forEach(element => {
-    element.addEventListener("click", () => {
-        footerNavList.forEach(el => el.classList.remove("highlighted"));
-        element.classList.add("highlighted");
-    })
+arrowRight.addEventListener("click", (e) => {
+  clearCards();
 });
+
+function sliderFade() {}
+function shuffle() {}
